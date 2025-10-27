5425aedb43565dbf51542f45cdd69a279bc04e71
yigit.yildirimoglu <yildirimglu@proksiyazilim.com>
2025-10-27 09:59:03 +0300

    root endpoint changed again and that is awesome

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ async def root():
     logger.info("Root endpoint '/' called.")
     return HealthResponse(
         status="ok",
-        message="Jenkins Demooo API is running! Heyoooooo!"
+        message="Jenkins Demooo API is running! Heyoooooo! and you are awesome!"
     )
 
 
